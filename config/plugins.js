dde8aeef426d0d03b9da3ebe9030417f7c091082
Marian Polyak <info@neo-lines.com>
2025-04-21 03:19:25 +0300

    Initial Strapi commit

---
+++ config/plugins.js
@@ -0,0 +1,14 @@
+module.exports = {
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+    },
+  },
+};
+
+
